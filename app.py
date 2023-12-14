677df7e79209dd23ce2c03e7301310608d232fc3
CharbelBT <94170926+CharbelBT@users.noreply.github.com>
2023-12-14 12:30:28 +0100

    Add files via upload
    Test 1

---
+++ app.py
@@ -0,0 +1,34 @@
+# app.py
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operation = request.form['operation']
+
+        result = perform_calculation(num1, num2, operation)
+        return render_template('result.html', result=result)
+
+def perform_calculation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by zero"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
